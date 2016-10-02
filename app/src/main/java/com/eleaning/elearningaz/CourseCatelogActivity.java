8018be5f0693446b37afacf18b1314525d2af252
ighotouch <ighotouch@gmail.com>
2016-10-02 07:57:38 +0100

    Merge branch 'master' into matthewWorkstation
    # Conflicts:
    #	app/build.gradle

--- app/src/main/java/com/eleaning/elearningaz/CourseCatelogActivity.java
+++ app/src/main/java/com/eleaning/elearningaz/CourseCatelogActivity.java
@@ -3,6 +3,7 @@ package com.eleaning.elearningaz;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,6 +16,7 @@ import android.view.MenuItem;
 
 public class CourseCatelogActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    RecyclerView categoryRecycleView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +24,6 @@ public class CourseCatelogActivity extends AppCompatActivity
         setContentView(R.layout.activity_course_catelog);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
 
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -33,6 +34,8 @@ public class CourseCatelogActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        categoryRecycleView = (RecyclerView) findViewById(R.id.category_recycler_view);
 
         //https://github.com/daimajia/AndroidImageSlider
     }
---
+++ app/src/main/java/com/eleaning/elearningaz/utils/rest/model/course/CourseCategory.java
@@ -0,0 +1,8 @@
+package com.eleaning.elearningaz.utils.rest.model.course;
+
+/**
+ * Created by ighotouch on 02/10/2016.
+ */
+
+public class CourseCategory {
+}
